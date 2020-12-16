92f5a34f3b445175d4394ed604f0b6f884c654ae
Andra Malmen <andra.malmen@tieto.com>
2020-12-16 22:29:20 +0200

    Add Icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Feather';
+
 const SearchBar = () => {
     return (
         <View style={styles.background}>
+            <Icon name="search" size={30} />
             <Text>SearchBar</Text>
         </View>
     );
